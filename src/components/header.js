b64271df9538cc24e3be7839918e1fb7d60f78aa
Pierce Tickle <pierce.tickle@gmail.com>
2020-06-13 16:44:36 -0400

    got some dummy data output

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,13 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import containerStyles from "./header.module.css"
+
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header className={containerStyles.headerContainer}>
+    <div className={containerStyles.header}>
+      <h1>
         <Link
           to="/"
           style={{
